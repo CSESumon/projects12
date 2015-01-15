86460e0e81fd8df12e1ea41f369b6f4076527c3b
CSESumon <sacsesumon@gmail.com>
2015-01-15 10:43:21 +0600

    21 dec

---
+++ AccountApp/AccountApp/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountApp
+{
+    class Account
+    {
+        public string number;
+        public string name;
+        public double balance;
+
+        public string Deposit(double amount)
+        {
+            balance += amount;
+            return "Deposited";
+        }
+
+        public string Withdraw(double amount)
+        {
+            balance -= amount;
+            return "Withdrawn";
+        }
+
+        public string GetReport()
+        {
+            return name + ", your account no is " + number + " and it's balance is : " + balance;
+        }
+    }
+}
---
+++ AccountApp/AccountApp/AccountUI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountApp
+{
+    public partial class AccountUI : Form
+    {
+        private Account anAccount = new Account();
+        private double amount;
+
+        public AccountUI()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            anAccount.name = nameTextBox.Text;
+            anAccount.number = numberTextBox.Text;
+            MessageBox.Show("Account has been created");
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            amount = Convert.ToDouble(amountTextBox.Text);
+            anAccount.Deposit(amount);
+            MessageBox.Show("Deposited");
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            amount = Convert.ToDouble(amountTextBox.Text);
+            anAccount.Withdraw(amount);
+            MessageBox.Show("Withdraw");
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(anAccount.GetReport());
+        }
+    }
+}
